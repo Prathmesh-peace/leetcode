cdd0384ea81f0349c672647d9244f1f49d034a22
Prathmesh <prathmeshking45@gmail.com>
2026-07-12 19:25:38 +0530

    Add problems/0081-search-in-rotated-sorted-array-ii/java/Solution.java

--- problems/0081-search-in-rotated-sorted-array-ii/java/Solution.java
+++ problems/0081-search-in-rotated-sorted-array-ii/java/Solution.java
@@ -1,7 +1,21 @@
 class Solution {
     public boolean search(int[] nums, int x) {
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == x) return true;
+        int n = nums.length, i = 0, j = n-1;
+        while(i <= j){
+            int mid = i + (j-i)/2;
+            if(nums[mid] == x) return true;
+            else if(nums[i] == nums[mid] && nums[j] == nums[mid]) {
+                i++;
+                j--;
+            }
+            else if(nums[i] <= nums[mid]){
+                if(nums[i] <= x && x < nums[mid]) j = mid-1;
+                else i = mid + 1;
+            }
+            else{
+                if(nums[mid] < x && x <= nums[j]) i = mid+1;
+                else j = mid-1;
+            } 
         }
         return false;
     }
