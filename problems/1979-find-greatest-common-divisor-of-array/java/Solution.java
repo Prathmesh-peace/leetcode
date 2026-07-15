876d2259534de908cada035471b44afab351e959
Prathmesh <prathmeshking45@gmail.com>
2026-07-15 11:15:31 +0530

    Add problems/1979-find-greatest-common-divisor-of-array/java/Solution.java

---
+++ problems/1979-find-greatest-common-divisor-of-array/java/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findGCD(int[] nums) {
+        int a = Integer.MIN_VALUE, b = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            a = Math.max(a, nums[i]);
+            b = Math.min(b, nums[i]);
+        }
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.abs(a);
+    }
+}
