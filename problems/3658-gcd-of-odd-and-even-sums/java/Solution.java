0c9f817ecc6a0385c5dac29fbdc6121eb655b0f0
Prathmesh <prathmeshking45@gmail.com>
2026-07-15 11:08:05 +0530

    Add problems/3658-gcd-of-odd-and-even-sums/java/Solution.java

--- problems/3658-gcd-of-odd-and-even-sums/java/Solution.java
+++ problems/3658-gcd-of-odd-and-even-sums/java/Solution.java
@@ -1,11 +1,5 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int sumOdd = n*n, sumEven = n * (n+1);
-        while(sumEven != 0){
-            int temp = sumEven;
-            sumEven = sumOdd % sumEven;
-            sumOdd = temp;
-        }
-        return Math.abs(sumOdd);
+        return n;
     }
 }
