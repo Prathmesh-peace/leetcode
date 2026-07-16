b4c964a4eb05df9d1d6d87c540cbbc02dde8ca3c
Prathmesh <prathmeshking45@gmail.com>
2026-07-16 12:00:11 +0530

    Add problems/0633-sum-of-square-numbers/java/Solution.java

--- problems/0633-sum-of-square-numbers/java/Solution.java
+++ problems/0633-sum-of-square-numbers/java/Solution.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        int n = (int) Math.floor(Math.sqrt(c)), i = 1, j = n;
-        if(n*n == c) return true;
+        int n = (int) Math.sqrt(c), i = 0, j = n;
         while(i <= j){
             long sum = 1L*i*i + 1L*j*j;
             if(sum == c) return true;
