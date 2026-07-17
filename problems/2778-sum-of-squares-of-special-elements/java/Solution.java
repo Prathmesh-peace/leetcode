83d6911096f7162f82fea5db9614161399942a1a
Prathmesh <prathmeshking45@gmail.com>
2026-07-17 12:14:20 +0530

    Add problems/2778-sum-of-squares-of-special-elements/java/Solution.java

---
+++ problems/2778-sum-of-squares-of-special-elements/java/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int sumOfSquares(int[] nums) {
+        int sum = 0, n = nums.length;
+        for(int i = 1; i < n + 1; i++){
+            if(n % i == 0) sum += nums[i-1]*nums[i-1];
+        }
+        return sum;
+    }
+}
