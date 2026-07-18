b6b072cd7c216dc699d24441fb267052eaa6d8db
Prathmesh <prathmeshking45@gmail.com>
2026-07-18 19:04:52 +0530

    Add problems/0300-longest-increasing-subsequence/java/Solution.java

--- problems/0300-longest-increasing-subsequence/java/Solution.java
+++ problems/0300-longest-increasing-subsequence/java/Solution.java
@@ -2,8 +2,8 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int maxlen = 0;
         int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
         for(int i = 0; i < nums.length; i++){
+            dp[i] = 1;
             for(int j = i-1; j >= 0; j--){
                 if(nums[j] < nums[i]){
                     dp[i] = Math.max(dp[i], 1 + dp[j]);
