04683e958c73b0b48af0987400f46d8f1f29291a
Prathmesh <prathmeshking45@gmail.com>
2026-07-22 15:10:20 +0530

    Add problems/0169-majority-element/java/Solution.java

--- problems/0169-majority-element/java/Solution.java
+++ problems/0169-majority-element/java/Solution.java
@@ -1,6 +1,14 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int ele : nums){
+            if(map.containsKey(ele)) map.put(ele, map.get(ele) + 1);
+            else map.put(ele, 1);
+        }
+        for(int key : map.keySet()){
+            if(map.get(key) > n/2) return key;
+        }
+        return 0;
     }
 }
