306da7dc56c0a0fef7d5dddb306a35a665ce6e4e
Prathmesh <prathmeshking45@gmail.com>
2026-07-22 19:42:47 +0530

    Add problems/0088-merge-sorted-array/java/Solution.java

--- problems/0088-merge-sorted-array/java/Solution.java
+++ problems/0088-merge-sorted-array/java/Solution.java
@@ -1,9 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int x = nums1.length;
-        for(int i = 0; i < n; i++){
-            nums1[x-n+i] = nums2[i];
+        int i = m-1, j = n-1, k = m+n-1;
+        while(i >= 0 && j >= 0){
+            if(nums2[j] > nums1[i]){
+                nums1[k] = nums2[j];
+                j--; k--;
+            }
+            else{
+                nums1[k] = nums1[i];
+                i--; k--;
+            }
         }
-        Arrays.sort(nums1);
+        while(j >= 0){
+            nums1[k] = nums2[j];
+            j--; k--;
+        }
     }
 }
