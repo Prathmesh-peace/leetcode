2aedc678bd5dcaecb49c9f57888cd39e5322a068
Prathmesh <prathmeshking45@gmail.com>
2026-07-22 14:34:09 +0530

    Archive Two Sum (java)

---
+++ problems/0001-two-sum/java/history/2026-07-22_14-32-52_2076883677/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int rem = target - nums[i];
+            if(map.containsKey(rem)) return new int[]{i, map.get(rem)};
+            else map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
