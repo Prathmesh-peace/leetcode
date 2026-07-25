91bcb7e20b412885fbb748e01ef6160f49103533
Prathmesh <prathmeshking45@gmail.com>
2026-07-25 23:25:32 +0530

    Add problems/0242-valid-anagram/java/Solution.java

--- problems/0242-valid-anagram/java/Solution.java
+++ problems/0242-valid-anagram/java/Solution.java
@@ -1,13 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false ;
-        char[] a = s.toCharArray();
-        char[] b = t.toCharArray();
-        Arrays.sort(a); Arrays.sort(b);
-        for(int i = 0 ; i < a.length ; i++) {
-            if (a[i] != b[i]) return false ;
-            
-        }
-        return true ;
+        int[] ans = new int[26];
+        for(int i = 0; i < s.length(); i++) ans[s.charAt(i) - 'a']++;
+        for(int i = 0; i < t.length(); i++) ans[t.charAt(i) - 'a']--;
+        for(int i = 0; i < 26; i++) if(ans[i] != 0) return false;
+        return true;
     }
 }
