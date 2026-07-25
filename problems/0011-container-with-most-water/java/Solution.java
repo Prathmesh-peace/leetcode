a4af100da6722b5be1c9be801c5178b3dbcdf70f
Prathmesh <prathmeshking45@gmail.com>
2026-07-25 22:43:47 +0530

    Add problems/0011-container-with-most-water/java/Solution.java

---
+++ problems/0011-container-with-most-water/java/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length, i = 0, j = n-1, area = 0;
+        while(i < j){
+            area = Math.max(area, Math.min(height[i], height[j]) * (j-i));
+            if(height[i] < height[j]) i++;
+            else j--;
+        }
+        return area;
+    }
+}
