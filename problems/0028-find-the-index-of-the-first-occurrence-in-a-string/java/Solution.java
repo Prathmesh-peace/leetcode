bd99b45d9374ad23c041067b1bc5287541e37eea
Prathmesh <prathmeshking45@gmail.com>
2026-07-28 21:49:14 +0530

    Add problems/0028-find-the-index-of-the-first-occurrence-in-a-string/java/Solution.java

--- problems/0028-find-the-index-of-the-first-occurrence-in-a-string/java/Solution.java
+++ problems/0028-find-the-index-of-the-first-occurrence-in-a-string/java/Solution.java
@@ -1,12 +1,34 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int n = needle.length(), m = haystack.length(), ans = -1;
-        if(m >= n){
-            for(int i = 0; i < m-n+1; i++){
-                String s = haystack.substring(i, i+n);
-                if(s.equals(needle)) return i;
+        // knuth-morris-pratt (KMP) algorithm
+        int m = haystack.length(), n = needle.length();
+        int[] lps = new int[n]; // for longest prefix-suffix
+        int length = 0, i = 1, j = 0;
+        while(i < n){
+            if(needle.charAt(i) == needle.charAt(length)){
+                length++;
+                lps[i] = length;
+                i++;
+            }
+            else{
+                if(length != 0) length = lps[length - 1];
+                else{
+                    lps[i] = 0;
+                    i++;
+                }
             }
         }
-        return -1;
+        i = 0;
+        while(i < m && j < n){
+            if(haystack.charAt(i) == needle.charAt(j)){
+                i++;
+                j++;
+            }
+            else{
+                if(j != 0) j = lps[j-1];
+                else i++;
+            }
+        }
+        return (j == n) ? i-j : -1;
     }
 }
