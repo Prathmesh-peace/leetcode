36d413724ead2d27c85c0e9485a948e0a0b5ecc8
Prathmesh <prathmeshking45@gmail.com>
2026-07-29 20:55:27 +0530

    Add problems/0686-repeated-string-match/java/Solution.java

--- problems/0686-repeated-string-match/java/Solution.java
+++ problems/0686-repeated-string-match/java/Solution.java
@@ -1,13 +1,46 @@
 class Solution {
+    public boolean kmp(int i, int j, String a, String b, int[] lps){
+        int m = a.length(), n = b.length();
+        while(i < m && j < n){
+            if(a.charAt(i) == b.charAt(j)){
+                i++;
+                j++;
+            }
+            else{
+                if(j != 0) j = lps[j-1];
+                else i++;
+            }
+        }
+        return j == n;
+    }
     public int repeatedStringMatch(String a, String b) {
+        int count = 1, length = 0, i = 1;
+        int[] lps = new int[b.length()];
+        while(i < b.length()){
+            if(b.charAt(i) == b.charAt(length)){
+                length++;
+                lps[i] = length;
+                i++;
+            }
+            else{
+                if(length != 0) length = lps[length - 1];
+                else{
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
         String original = a;
-        int count = 1;
         while(b.length() > a.length()){
             a += original;
             count++;
         }
-        if(a.contains(b)) return count;
-        a += original;
-        return (a.contains(b)) ? count+1 : -1;
+        boolean flag = kmp(0, 0, a, b, lps);
+        if(flag) return count;
+        else{
+            a += original;
+            flag = kmp(0, 0, a, b, lps);
+        }
+        return (flag) ? count+1 : -1;
     }
 }
