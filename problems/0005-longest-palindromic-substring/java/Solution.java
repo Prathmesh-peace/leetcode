f7179ffd99d34aff4a563909a14aa0b66d1fe2e1
Prathmesh <prathmeshking45@gmail.com>
2026-07-30 18:53:09 +0530

    Add problems/0005-longest-palindromic-substring/java/Solution.java

--- problems/0005-longest-palindromic-substring/java/Solution.java
+++ problems/0005-longest-palindromic-substring/java/Solution.java
@@ -1,24 +1,21 @@
 class Solution {
-    public boolean palindrome(int i, int j, String s){
-        while(i <= j){
-            if(s.charAt(i) != s.charAt(j)) return false;
-            i++;
-            j--;
+    static int x, y;
+    public void expand(int i, int j, String s){
+        while(i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)){
+            i--;
+            j++;
         }
-        return true;
+        if(j-i-1 > y-x+1){  
+            x = i+1;
+            y = j-1;
+        }
     }
     public String longestPalindrome(String s) {
-        int x = 0, y = 0, len = 0;
-        for(int i = 0; i < s.length(); i++){
-            for(int j = i; j < s.length(); j++){
-                if(palindrome(i, j, s)){
-                    if(j-i+1 > len){
-                        len = j-i+1;
-                        x = i;
-                        y = j;
-                    }
-                }
-            }
+        x = 0; y = 0;
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            expand(i, i, s); // expand from center for odd length
+            expand(i, i+1, s); // expand from center for even length
         }
         return s.substring(x, y+1);
     }
