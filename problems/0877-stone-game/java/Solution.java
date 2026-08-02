bfabb83b9e52961c3c674e69375a52f2d3d13cc7
Prathmesh <prathmeshking45@gmail.com>
2026-08-02 12:06:48 +0530

    Add problems/0877-stone-game/java/Solution.java

--- problems/0877-stone-game/java/Solution.java
+++ problems/0877-stone-game/java/Solution.java
@@ -9,17 +9,7 @@ class Solution {
             i++;
             j--;
         }
-        if(alice > bob) return true;
-        bob = Integer.MIN_VALUE; alice = Integer.MAX_VALUE;
-        i = 0; j = piles.length-1;
-        while(i < j){
-            int max = Math.max(piles[i], piles[j]);
-            int min = Math.min(piles[i], piles[j]);
-            alice += min;
-            bob += max;
-            i++;
-            j--;
-        }
+        
         return alice > bob;
     }
 }
