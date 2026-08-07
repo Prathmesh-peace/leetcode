d2295b73e086f3460780dc325712cb1720772e3c
Prathmesh <prathmeshking45@gmail.com>
2026-08-07 20:12:05 +0530

    Archive Subarray Sums Divisible by K (java)

---
+++ problems/0974-subarray-sums-divisible-by-k/java/history/2026-08-07_20-09-53_2098065593/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 1; i < nums.length; i++) nums[i] += nums[i-1];
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] % k == 0) count++;
+            int rem = ((nums[i] % k) + k) % k;
+            if(map.containsKey(rem)) count += map.get(rem);
+            if(map.containsKey(rem)) map.put(rem, map.get(rem) + 1);
+            else map.put(rem, 1);
+        }
+        return count;
+    }
+}
