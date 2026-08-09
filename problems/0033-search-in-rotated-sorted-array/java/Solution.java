1af1503af84c3e9c52feef0da38be0fe3af5818f
Prathmesh <prathmeshking45@gmail.com>
2026-08-09 14:46:23 +0530

    Add problems/0033-search-in-rotated-sorted-array/java/Solution.java

--- problems/0033-search-in-rotated-sorted-array/java/Solution.java
+++ problems/0033-search-in-rotated-sorted-array/java/Solution.java
@@ -9,15 +9,14 @@ class Solution {
         return -1;
     }
     public int search(int[] nums, int target) {
-        int pivot = -1;
-        for(int i = 0; i < nums.length - 1; i++){
-            if(nums[i] > nums[i+1]) {
-                pivot = i;
-                break;
-            }
+        int i = 0, j = nums.length-1;
+        while(i < j){
+            int mid = i + (j-i)/2;
+            if(nums[mid] > nums[j]) i = mid + 1;
+            else j = mid;
         }
-        int a = bs(0, pivot, target, nums);
-        int b = bs(pivot+1, nums.length-1, target, nums);
+        int a = bs(0, j-1, target, nums);
+        int b = bs(j, nums.length-1, target, nums);
         return (a == -1 && b == -1) ? -1 : Math.max(a,b);
     }
 }
