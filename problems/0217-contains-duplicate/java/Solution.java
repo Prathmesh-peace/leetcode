5571c1134ac93adebff155d60645b5f70de37b94
Prathmesh <prathmeshking45@gmail.com>
2026-08-26 11:32:48 +0530

    Add problems/0217-contains-duplicate/java/Solution.java

--- problems/0217-contains-duplicate/java/Solution.java
+++ problems/0217-contains-duplicate/java/Solution.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> s = new HashSet<>();
-        for(int ele : nums){
-            if(s.contains(ele)) return true;
-            s.add(ele);
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i-1]) return true;
         }
         return false;
     }
